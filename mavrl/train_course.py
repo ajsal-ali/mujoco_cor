9f02afc5e1090dd64a7ecba621bf1681f0132a19
ajsal-ali <141856147+ajsal-ali@users.noreply.github.com>
2026-08-10 20:31:23 +0530

    bullshit

--- mavrl/train_course.py
+++ mavrl/train_course.py
@@ -27,7 +27,7 @@ from pathlib import Path
 import numpy as np
 import torch
 
-os.environ.setdefault("MUJOCO_GL", "egl")
+# os.environ.setdefault("MUJOCO_GL", "egl")
 sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 from mavrl import config as C                                    # noqa: E402
